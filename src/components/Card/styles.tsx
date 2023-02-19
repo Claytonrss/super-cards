d3e3a5e3e9c224e35ee4c101c9f6c162a1d82620
Clayton Souza <clayton.rssouza@gmail.com>
2023-02-19 13:35:16 -0300

    feat: improve load css card

--- src/components/Card/styles.tsx
+++ src/components/Card/styles.tsx
@@ -9,25 +9,12 @@ export const CardStyle = styled.div<CardProps>`
   --color2: ${(props) => props.color2};
   --imgFront: ${(props) => `url('${props.imgFront}')`};
 
-  width: 500px;
-  height: 700px;
-  position: relative;
-  overflow: hidden;
-  margin: 20px;
-  z-index: 10;
-
   border-radius: 5% / 3.5%;
   box-shadow: -5px -5px 5px -5px var(--color1), 5px 5px 5px -5px var(--color2),
     -7px -7px 10px -5px transparent, 7px 7px 10px -5px transparent,
     0 0 5px 0px rgba(255, 255, 255, 0), 0 55px 35px -20px rgba(0, 0, 0, 0.5);
 
-  animation: none;
-  transition: all 0.1s ease-out;
-  will-change: transform, filter, box-shadow;
-
   background-image: var(--imgFront);
-  background-size: cover;
-  transform-origin: center;
 
   &:hover {
     box-shadow: -20px -20px 30px -25px var(--color1),
@@ -36,26 +23,18 @@ export const CardStyle = styled.div<CardProps>`
       0 55px 35px -20px rgba(0, 0, 0, 0.5);
   }
 
-  &:before {
-    content: '';
-    width: 100%;
-    aspect-ratio: 0.714;
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    top: 0;
-    background-repeat: no-repeat;
-    opacity: 0.5;
-    transition: all 0.33s ease;
-  }
-
   &.moved {
     transform: rotateX(var(--boxBeforeRotationY, 0))
       rotateY(var(--boxBeforeRotationX, 0));
   }
 `
 export const Wrapper = styled(CardStyle)`
-  ${holo1Style}
-  ${holo2Style}
+  ${({ hoverStyle }) => {
+    if (hoverStyle === 'holo1') {
+      return holo1Style
+    }
+    if (hoverStyle === 'holo2') {
+      return holo2Style
+    }
+  }}
 `
